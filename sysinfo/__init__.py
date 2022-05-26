90d26a0c045d6e3873dd62a4d774e9c93c463b56
PerfBleu <lunaticstarksm@gmail.com>
2022-05-26 15:43:46 +0800

    main

---
+++ sysinfo/__init__.py
@@ -0,0 +1,41 @@
+__version__ = '0.1.0'
+import os
+from PIL import Image, ImageFont, ImageDraw
+from hoshino import util, R
+from hoshino import Service
+
+path = os.getcwd()
+sv = Service('sysinfo', visible= True, enable_on_default= True, bundle='sysinfo', help_='''
+显示系统信息(screenfetch)
+'''.strip())
+def generate():
+    logo_data = os.popen('screenfetch -L -N') 
+    logo_text = logo_data.read()
+    info_data = os.popen('screenfetch -n -N') 
+    info_text = info_data.read()
+    hi_mem_proc_data = os.popen(' ps aux | head -1;ps aux|sort -k4nr|head -15') 
+    hi_mem_proc_text = hi_mem_proc_data.read()
+    hi_mem_proc=''
+    hmplist = hi_mem_proc_text.splitlines()
+    for line in hmplist:
+        hi_mem_proc = hi_mem_proc+line.replace(line[66:74],line[66:74]+'\n')+'\n'
+
+    image = Image.open(f'{path}/hoshino/modules/sysinfo/bg.png') # 读取图片
+    font = ImageFont.truetype(f'{path}/hoshino/modules/sysinfo/IBMPlexMono-MediumItalic.ttf',22) # 设置字体及字号
+    draw = ImageDraw.Draw(image)
+    flex = 750
+    #手工调的文字位置，加个变量方便调，换图的画自己调吧（笑
+    draw.text((flex+10,60), logo_text, '#f88066', font)
+    draw.text((flex+490,130), info_text, '#f88066', font)
+    draw.text((flex+230,560), hi_mem_proc, '#f88066', font)
+    image.save(f'{path}/hoshino/modules/sysinfo/tmp.png') # 保存图片
+    return('generate success')
+
+@sv.on_fullmatch(('screenfetch'))
+async def sysinfo(bot, ev):
+    try:
+        sv.logger.info(generate())
+    except:
+        sv.logger.warning('图片创建失败')
+    msg = f"[CQ:image,file=file:///{path}/hoshino/modules/sysinfo/tmp.png]"
+    await bot.send(ev, msg, at_sender=True)
